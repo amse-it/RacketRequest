8024c974dea86bf512471413be23baba33fc8c76
Virgil John A. Lomocso <vjlomocso@gmail.com>
2015-06-29 20:43:35 +0800

    wrote some tests

--- site_core/tests.py
+++ site_core/tests.py
@@ -6,30 +6,36 @@ import datetime
 
 
 class RequestTests(TestCase):
-    user = UserProfile(username='user', password='123456')
-
-    def setUp(self):
-        self.user.save()
-
-    def test_expiry_calculation(self):
-        now = timezone.now()
-        r = Request(
-            title='Test Request',
-            description='Make me a car',
-            duration=10,
-            created=now,
-            owner=self.user
-        )
-        self.assertEqual(r.expiry - now, datetime.timedelta(days=10))
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = UserProfile.objects.create(username='user', password='1234')
 
     def test_is_expired_with_an_old_request(self):
+        """
+        `is_expired` should return `True` for a request that is older than it's
+        duration.
+        """
         time = timezone.now() - datetime.timedelta(days=30)
         r = Request(
             title='Test Request',
             description='Make me a car',
             duration=10,
-            created=time,
+            created=time,  # r is not saved so created is not replaced
             owner=self.user,
         )
 
         self.assertTrue(r.is_expired)
+
+    def test_is_expired_with_a_recent_request(self):
+        """
+        `is_expired` should return `False` for a request that is younger than
+        it's duration.
+        """
+        r = Request(
+            title='Test Request',
+            description='Make me a song',
+            duration=10,
+            owner=self.user,
+        )
+        r.save()
+        self.assertFalse(r.is_expired)
