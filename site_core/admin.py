cfc7468009b7b89ed8a552c18d6c73701e4e0dfb
Virgil John A. Lomocso <vjlomocso@gmail.com>
2015-06-29 08:02:06 +0800

    reregistered the extended User model

--- site_core/admin.py
+++ site_core/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
 from site_core.models import (Racket, Request, Upload, Comment, Rating,
-    Reputation)
+    Reputation, UserProfiles)
+
+admin.site.unregister(User)
+admin.site.register(UserProfiles)
 
 admin.site.register(Racket)
 admin.site.register(Request)
